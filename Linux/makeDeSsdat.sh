2dad04f879576e066c2b5c165707124ff99d22a2
sonofevil <debilerpc@web.de>
2023-07-03 12:09:32 +0200

    add multi-argument parsing

--- Linux/makeDeSsdat.sh
+++ Linux/makeDeSsdat.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
+#
+# makeDeSsdat.sh
+#
+# Reads arguments as file paths and converts files to Demon's Souls compatible .sdat using make_npdata.
+# make_npdata binary must be in the same directory as script.
 
-inputFile="$1"
-outputFile="$inputFile.sdat"
-cmdDir="$(dirname "`readlink -f "$0"`")"
+cmdDir="$(dirname "$(readlink -f "$0")")"
 
-"$cmdDir/make_npdata" -e "$inputFile" "$outputFile" SDAT 1 2 0 16 0 00 XXYYYY-AAAABBBBB_CC-DDDDDDDDDDDDDDDD 4
+# 'make_npdata -e' settings for Demon's Souls sdat format.
+# This is the only game-specific line of the script.
+encSettings="SDAT 1 2 0 16 0 00 XXYYYY-AAAABBBBB_CC-DDDDDDDDDDDDDDDD 4"
+
+for inputFile in "$@"; do
+  if test -f "$inputFile"; then
+    outputFile="$inputFile.sdat"
+    "$cmdDir/make_npdata" -e "$inputFile" "$outputFile" $encSettings
+  else
+    echo "File not found: $inputFile"
+  fi
+done
+
+echo "Job done."
